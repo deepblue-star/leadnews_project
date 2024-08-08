b5194021ecdabee28bcb81ffb4c4b6212064f53a
PeterGriffin <872571506@qq.com>
2024-08-08 14:39:31 +0800

    day04

--- heima-leadnews-common/src/main/java/com/heima/common/aliyun/GreenImageScan.java
+++ heima-leadnews-common/src/main/java/com/heima/common/aliyun/GreenImageScan.java
@@ -4,8 +4,22 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Getter
 @Setter
 @Component
 public class GreenImageScan {
+
+    private String accessKeyId;
+    private String secret;
+    private String scenes;
+
+    public Map imageScan(List<byte[]> imageList) throws Exception {
+        Map<String, String> resultMap = new HashMap<>();
+        resultMap.put("suggestion", "pass");
+        return resultMap;
+    }
 }
--- heima-leadnews-common/src/main/java/com/heima/common/aliyun/GreenTextScan.java
+++ heima-leadnews-common/src/main/java/com/heima/common/aliyun/GreenTextScan.java
@@ -4,8 +4,21 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @Setter
 @Component
 public class GreenTextScan {
+
+    private String accessKeyId;
+    private String secret;
+
+    public Map greeTextScan(String content) throws Exception {
+        Map<String, String> resultMap = new HashMap<>();
+        resultMap.put("suggestion", "pass");
+        return resultMap;
+    }
+
 }
